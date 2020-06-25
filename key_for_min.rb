e940bf5a1bde3267f87cd5191eabd3731eedf067
Brian Lego <legobw@gmail.com>
2020-06-25 21:30:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
-  output_key = ""
+  output_key = nil 
   key_for_min_value.each do |key, value|
-    
-  
+    if value<output_key
+      output_key << key 
+    end
   end
   output_key
 end
